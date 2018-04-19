633b020b589540cd1e3cd96022cd58a7b13e211a
Ankit Rahaman <32190102+rahamanankit@users.noreply.github.com>
2018-04-19 23:43:56 +0530

    I have modified the splash screen with a progress bar..

--- app/src/main/java/in/paperwrk/safetycollabproject/SplashActivity.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/SplashActivity.java
@@ -1,11 +1,13 @@
 package in.paperwrk.safetycollabproject;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +22,7 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        },2000);
+        },5000);
 
     }
 
