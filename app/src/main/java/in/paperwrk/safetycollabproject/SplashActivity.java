71b3fd3af131d5495b09ea76a512d3ab5825e9f1
kkdroidgit <kartikdroid17@gmail.com>
2018-04-19 09:26:34 +0530

    Added Handler to Splash Activity to navigate to Home Activity

--- app/src/main/java/in/paperwrk/safetycollabproject/SplashActivity.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/SplashActivity.java
@@ -1,7 +1,9 @@
 package in.paperwrk.safetycollabproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        // go to MainActivity after a delay 2 seconds
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+
     }
+
+
 }
