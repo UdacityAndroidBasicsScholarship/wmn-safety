1ada4a4ce111ba98f8047b95f2b784abef80720f
Manthan Shah <mantyshah31@gmail.com>
2018-04-20 16:45:00 +0530

    Splash Screen Created. Added App Logo. Added Punch Line.

--- app/src/main/java/in/paperwrk/safetycollabproject/SplashActivity.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/SplashActivity.java
@@ -4,15 +4,44 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    TextView appName, punchLine;
+    ImageView appLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        // go to MainActivity after a delay 2 seconds
+        appLogo = findViewById(R.id.nirbheekLogo);
+        appName = findViewById(R.id.appName);
+        punchLine = findViewById(R.id.punchLine);
+
+        Animation animAppLogo = AnimationUtils.loadAnimation(SplashActivity.this, R.anim.scaleup);
+        animAppLogo.setInterpolator((new AccelerateDecelerateInterpolator()));
+        animAppLogo.setFillAfter(true);
+        appLogo.setAnimation(animAppLogo);
+
+        Animation animAppName = AnimationUtils.loadAnimation(SplashActivity.this, R.anim.animlefttoright);
+        animAppName.setInterpolator((new AccelerateDecelerateInterpolator()));
+        animAppName.setFillAfter(true);
+        appName.setAnimation(animAppName);
+
+        Animation animPunchLine = AnimationUtils.loadAnimation(SplashActivity.this, R.anim.animdowntoup);
+        animPunchLine.setInterpolator((new AccelerateDecelerateInterpolator()));
+        animPunchLine.setFillAfter(true);
+        punchLine.setAnimation(animPunchLine);
+
+
+
+        // go to MainActivity after a delay 5 seconds
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -20,7 +49,7 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        },2000);
+        },5000);
 
     }
 
