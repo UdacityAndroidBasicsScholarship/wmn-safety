b79225cc78420ab37d0a01c76d22b5e93002a414
bishram78 <bishram.munda78@gmail.com>
2018-04-22 12:17:23 +0530

    Animation to logo has been added. Fade in and fade out animation introduced for ImageView (logo).

--- app/src/main/java/in/paperwrk/safetycollabproject/SplashActivity.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/SplashActivity.java
@@ -1,20 +1,31 @@
 package in.paperwrk.safetycollabproject;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private int SPLASH_TIME_OUT = 2000;
+    private ImageView ivSplashLogo;
+
+    private int SPLASH_TIME_OUT = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        // go to MainActivity after a delay 2 seconds
+        ivSplashLogo = findViewById(R.id.iv_spl_wmn_sfty_logo);
+        ivSplashLogoAnimation();
+
+        // go to MainActivity after a delay 5 seconds
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -23,6 +34,24 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
             }
         }, SPLASH_TIME_OUT);
+    }
 
+    public void ivSplashLogoAnimation() {
+        ObjectAnimator fadeOut = ObjectAnimator.ofFloat(ivSplashLogo, "alpha",  1f, 0.1f);
+        fadeOut.setDuration(100);
+        ObjectAnimator fadeIn = ObjectAnimator.ofFloat(ivSplashLogo, "alpha", 0.1f, 1f);
+        fadeIn.setDuration(500);
+
+        final AnimatorSet mAnimationSet = new AnimatorSet();
+
+        mAnimationSet.play(fadeIn).after(fadeOut);
+        mAnimationSet.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                mAnimationSet.start();
+            }
+        });
+        mAnimationSet.start();
     }
 }
