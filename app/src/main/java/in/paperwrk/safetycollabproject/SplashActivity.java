443a3ae2f8467e27ef372d8757499f5624f408d4
Akshay <akshay.talwar123@gmail.com>
2018-04-22 08:43:29 +0530

    Splash Screen by Akshay Talwar

--- app/src/main/java/in/paperwrk/safetycollabproject/SplashActivity.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/SplashActivity.java
@@ -4,25 +4,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        progressBar = findViewById(R.id.progress);
+
         // go to MainActivity after a delay 2 seconds
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                doWork();
                 Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
         },2000);
 
-        
+
     }
 
-}
+    private void doWork() {
+
+            for (int progress=0; progress<100; progress+=10) {
+
+                progressBar.setProgress(progress);
+            }
+        }
+    }
+
+
