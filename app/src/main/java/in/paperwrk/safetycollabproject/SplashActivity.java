6ec6d27e934a068699bb0080d0898caf583932e1
dkmehta <dkmehta124@gmail.com>
2018-04-24 00:28:56 +0530

    Splash Screen done

--- app/src/main/java/in/paperwrk/safetycollabproject/SplashActivity.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/SplashActivity.java
@@ -4,25 +4,46 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+    private TextView tv;
+    private ImageView iv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        // go to MainActivity after a delay 2 seconds
-        new Handler().postDelayed(new Runnable() {
-            @Override
+        tv = (TextView) findViewById(R.id.tv);
+        iv = (ImageView) findViewById(R.id.iv);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.mytransition);
+        tv.startAnimation(myanim);
+        iv.startAnimation(myanim);
+        final Intent i = new Intent(this,MainActivity.class);
+        Thread timer = new Thread() {
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        },2000);
+                try {
+                    sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(i);
+                    finish();
 
+                }
+            }
+        };
+        timer.start();
 
     }
+
+
+
 
 }
