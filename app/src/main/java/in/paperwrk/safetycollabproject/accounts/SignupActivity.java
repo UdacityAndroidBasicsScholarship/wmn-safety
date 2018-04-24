08d23372911708e507faad17720753b703c748fa
vinay <vinay.gupta@ssntpl.com>
2018-04-24 21:27:03 +0530

    add firebase authentication, and butterknife

---
+++ app/src/main/java/in/paperwrk/safetycollabproject/accounts/SignupActivity.java
@@ -0,0 +1,15 @@
+package in.paperwrk.safetycollabproject.accounts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import in.paperwrk.safetycollabproject.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
