98c3e293bd52c4dc49777792fca9892e9801fd62
piedcipher <pateltirth454@gmail.com>
2018-04-26 23:15:48 +0530

    Refactored MainActivity to HomeActivity

--- app/src/main/java/in/paperwrk/safetycollabproject/SplashActivity.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/SplashActivity.java
@@ -11,18 +11,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-        // go to MainActivity after a delay 2 seconds
+        // go to HomeActivity after a delay 2 seconds
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
                 startActivity(intent);
                 finish();
             }
         },2000);
-
-
     }
-
 }
