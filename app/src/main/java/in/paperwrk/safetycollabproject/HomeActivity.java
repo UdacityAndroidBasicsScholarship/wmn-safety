2fa922b7c2b45c0435e715611875de49a017de49
piedcipher <pateltirth454@gmail.com>
2018-04-26 23:18:23 +0530

    Refactored activity_main to activity_home

--- app/src/main/java/in/paperwrk/safetycollabproject/HomeActivity.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/HomeActivity.java
@@ -11,7 +11,7 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
         startActivity(new Intent(HomeActivity.this, SigninActivity.class));
     }
 }
