68b2569fa1cb1391bbd4487171c187e8e5562bf0
piedcipher <pateltirth454@gmail.com>
2018-04-26 23:14:06 +0530

    Removed unused imports

---
+++ app/src/main/java/in/paperwrk/safetycollabproject/HomeActivity.java
@@ -0,0 +1,17 @@
+package in.paperwrk.safetycollabproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import in.paperwrk.safetycollabproject.accounts.SigninActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startActivity(new Intent(HomeActivity.this, SigninActivity.class));
+    }
+}
