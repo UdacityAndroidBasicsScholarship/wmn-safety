e7880602761c7386cb3834bbf06d2ab5f76f3df6
mantyshah <mantyshah31@gmail.com>
2018-04-27 18:17:03 +0530

    1) Designed Activity for Sign In, Sign Up, Forgot Password. 2) Added code in sign in activity to check email and password is entered or not. 3) Added code in sign up activity to check email, password, phone number and full name is entered or not. 4) Added code in forgotPassword activity to check if email is entred or not.

--- app/src/main/java/in/paperwrk/safetycollabproject/accounts/ForgotPasswordActivity.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/accounts/ForgotPasswordActivity.java
@@ -1,15 +1,41 @@
 package in.paperwrk.safetycollabproject.accounts;
 
+import android.nfc.NfcAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import in.paperwrk.safetycollabproject.R;
 
-public class ForgotPasswordActivity extends AppCompatActivity {
+public class ForgotPasswordActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText emailEditText;
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        emailEditText = findViewById(R.id.emailEditText);
+        submit = findViewById(R.id.submit);
+        submit.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(!emailEditText.getText().toString().isEmpty()){
+            Toast.makeText(ForgotPasswordActivity.this, "Email has been sent to you. Please click on the link in the email.", Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(ForgotPasswordActivity.this, "Please Enter Your Email.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/in/paperwrk/safetycollabproject/accounts/SigninActivity.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/accounts/SigninActivity.java
@@ -1,15 +1,83 @@
 package in.paperwrk.safetycollabproject.accounts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import in.paperwrk.safetycollabproject.HomeActivity;
 import in.paperwrk.safetycollabproject.R;
 
-public class SigninActivity extends AppCompatActivity {
+public class SigninActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText email, password;
+    TextView forgotPassword, signUp;
+    Button signIn, googleSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        email = findViewById(R.id.emailEditText);
+        password = findViewById(R.id.passwordEditText);
+        forgotPassword = findViewById(R.id.forgotPassword);
+        signUp = findViewById(R.id.createAccount);
+        signIn = findViewById(R.id.signInButton);
+        googleSignIn = findViewById(R.id.googleSignIn);
+
+        forgotPassword.setOnClickListener(this);
+        signUp.setOnClickListener(this);
+        signIn.setOnClickListener(this);
+        googleSignIn.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.signInButton:
+
+                if(!email.getText().toString().isEmpty())
+                {
+                    if (!password.getText().toString().isEmpty())
+                    {
+                        Intent i = new Intent(SigninActivity.this, HomeActivity.class);
+                        startActivity(i);
+                        finish();
+                    }
+                    else {
+                        Toast.makeText(SigninActivity.this, "Please Enter Your Password.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else {
+
+                    Toast.makeText(SigninActivity.this, "Please Enter Your Email.", Toast.LENGTH_SHORT).show();
+                }
+
+                break;
+            case R.id.forgotPassword:
+                Intent forgotPasswordIntent = new Intent(SigninActivity.this, ForgotPasswordActivity.class);
+                startActivity(forgotPasswordIntent);
+                break;
+            case R.id.createAccount:
+                Intent signUpIntent = new Intent(SigninActivity.this, SignupActivity.class);
+                startActivity(signUpIntent);
+                break;
+            case R.id.googleSignIn:
+
+                break;
+        }
+
+
     }
 }
