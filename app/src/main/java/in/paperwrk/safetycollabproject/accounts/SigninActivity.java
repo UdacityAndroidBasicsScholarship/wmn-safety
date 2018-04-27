babc2c10ac6a6ee47a36d91b543cf08b6bb0ea81
sabdar18 <developer.sabdar@gmail.com>
2018-04-27 13:09:38 +0530

    Complete SignInActivity User Interface

--- app/src/main/java/in/paperwrk/safetycollabproject/accounts/SigninActivity.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/accounts/SigninActivity.java
@@ -3,13 +3,21 @@ package in.paperwrk.safetycollabproject.accounts;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.common.SignInButton;
+
 import in.paperwrk.safetycollabproject.R;
 
 public class SigninActivity extends AppCompatActivity {
+
+    private SignInButton mGoogleSignInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        mGoogleSignInButton= findViewById(R.id.sign_in_goggle_button);
+        mGoogleSignInButton.setSize(SignInButton.SIZE_WIDE);
+        mGoogleSignInButton.setColorScheme(SignInButton.COLOR_DARK);
     }
 }
