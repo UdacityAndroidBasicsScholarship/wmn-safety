a261d7802671fc8836137aaa122bbdf10d7270f0
sabdar18 <developer.sabdar@gmail.com>
2018-04-27 14:54:47 +0530

    Complete ForgotPasswordActivity User Interface

--- app/src/main/java/in/paperwrk/safetycollabproject/accounts/ForgotPasswordActivity.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/accounts/ForgotPasswordActivity.java
@@ -2,14 +2,36 @@ package in.paperwrk.safetycollabproject.accounts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import in.paperwrk.safetycollabproject.R;
 
-public class ForgotPasswordActivity extends AppCompatActivity {
+public class ForgotPasswordActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button mForgotButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        mForgotButton = findViewById(R.id.forgot_button);
+        mForgotButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.forgot_button:
+                doForgotPassword();
+                break;
+        }
+    }
+
+    private void doForgotPassword() {
+        Toast.makeText(this, R.string.implementation_in_progress, Toast.LENGTH_SHORT).show();
     }
 }
