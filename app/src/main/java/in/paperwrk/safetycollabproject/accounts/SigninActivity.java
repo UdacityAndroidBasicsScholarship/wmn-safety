e775d9fdad333c5fcfc1f120073d77b894f927e4
sabdar18 <developer.sabdar@gmail.com>
2018-04-27 13:55:59 +0530

    Complete SignUpActivity User Interface

--- app/src/main/java/in/paperwrk/safetycollabproject/accounts/SigninActivity.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/accounts/SigninActivity.java
@@ -1,23 +1,81 @@
 package in.paperwrk.safetycollabproject.accounts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.SignInButton;
 
 import in.paperwrk.safetycollabproject.R;
 
-public class SigninActivity extends AppCompatActivity {
+public class SigninActivity extends AppCompatActivity implements View.OnClickListener {
 
     private SignInButton mGoogleSignInButton;
+    private Button mSignInButton;
+    private TextView mSignUpTextView;
+    private TextView mForgotPasswordTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
 
-        mGoogleSignInButton= findViewById(R.id.sign_in_goggle_button);
+        mGoogleSignInButton = findViewById(R.id.sign_in_goggle_button);
         mGoogleSignInButton.setSize(SignInButton.SIZE_WIDE);
         mGoogleSignInButton.setColorScheme(SignInButton.COLOR_DARK);
+
+        mSignInButton = findViewById(R.id.sign_in_button);
+        mSignUpTextView = findViewById(R.id.sign_in_register_tv);
+        mForgotPasswordTextView = findViewById(R.id.sign_in_forgot_pass_tv);
+
+
+        mGoogleSignInButton.setOnClickListener(this);
+        mSignInButton.setOnClickListener(this);
+        mSignUpTextView.setOnClickListener(this);
+        mForgotPasswordTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        switch (id) {
+            case R.id.sign_in_button:
+                doSignIn();
+                break;
+            case R.id.sign_in_goggle_button:
+                doGoogleSignIn();
+                break;
+            case R.id.sign_in_register_tv:
+                showSignUp();
+                break;
+            case R.id.sign_in_forgot_pass_tv:
+                showForgotPassword();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void showForgotPassword() {
+        Intent intent = new Intent(this, ForgotPasswordActivity.class);
+        startActivity(intent);
+    }
+
+    private void showSignUp() {
+        Intent intent = new Intent(this, SignupActivity.class);
+        startActivity(intent);
+    }
+
+    private void doGoogleSignIn() {
+        Toast.makeText(this, R.string.implementation_in_progress, Toast.LENGTH_SHORT).show();
+    }
+
+    private void doSignIn() {
+        Toast.makeText(this, R.string.implementation_in_progress, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/in/paperwrk/safetycollabproject/accounts/SignupActivity.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/accounts/SignupActivity.java
@@ -1,15 +1,53 @@
 package in.paperwrk.safetycollabproject.accounts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import in.paperwrk.safetycollabproject.R;
 
-public class SignupActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mSignUpButton;
+    private TextView mSignInTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        mSignUpButton = findViewById(R.id.sign_up_button);
+        mSignInTextView = findViewById(R.id.sign_up_login_tv);
+
+        mSignUpButton.setOnClickListener(this);
+        mSignInTextView.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.sign_up_button:
+                doSignUp();
+                break;
+            case R.id.sign_up_login_tv:
+                showSignIn();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void showSignIn() {
+        finish();
+    }
+
+    private void doSignUp() {
+        Toast.makeText(this, R.string.implementation_in_progress, Toast.LENGTH_SHORT).show();
     }
 }
