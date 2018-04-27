f5ef5e65db533bb9ed3a9c63cf7d0869b0e3b146
vipul Aggarwal <vipul.captureart@gmail.com>
2018-04-28 00:54:58 +0530

    Pull Request For Splash Screen

--- app/src/main/java/in/paperwrk/safetycollabproject/accounts/SigninActivity.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/accounts/SigninActivity.java
@@ -1,15 +1,22 @@
 package in.paperwrk.safetycollabproject.accounts;
 
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.support.v4.view.LayoutInflaterCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 import in.paperwrk.safetycollabproject.R;
+
 
 public class SigninActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signin);
+        setContentView(R.layout.activity_main);
     }
 }
+
