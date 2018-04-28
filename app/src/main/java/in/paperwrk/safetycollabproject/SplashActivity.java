91cbb80d257cc82522a399d7d8b8121245b7adbc
Gopal Dan <gopal125dan@gmail.com>
2018-04-28 19:53:49 +0530

    Added splash screen,sign in & sign up activity

--- app/src/main/java/in/paperwrk/safetycollabproject/SplashActivity.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/SplashActivity.java
@@ -1,28 +1,57 @@
 package in.paperwrk.safetycollabproject;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class SplashActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-
-        // go to MainActivity after a delay 2 seconds
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        },2000);
+import in.paperwrk.safetycollabproject.accounts.SigninActivity;
 
 
+public class SplashActivity extends Activity {
+
+        TextView txtMessage1,txtMessage2;
+
+        ImageView image;
+
+        // Animation
+        Animation animFadein;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+           // getActionBar().hide();
+            setContentView(R.layout.activity_splash);
+
+
+            txtMessage1 = (TextView) findViewById(R.id.text1);
+            txtMessage2 = (TextView) findViewById(R.id.text2);
+
+            image = (ImageView) findViewById(R.id.image);
+
+            // load the animation
+            animFadein = AnimationUtils.loadAnimation(getApplicationContext(),
+                    R.anim.slide_down);
+
+            // start the animation
+            txtMessage1.startAnimation(animFadein);
+            txtMessage2.startAnimation(animFadein);
+            image.startAnimation(animFadein);
+
+           //setting duration of animation
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    final Intent mainIntent = new Intent(SplashActivity.this, SigninActivity.class);
+                    SplashActivity.this.startActivity(mainIntent);
+                    SplashActivity.this.finish();
+                }
+            }, 3000);
+        }
     }
-
-}
--- app/src/main/java/in/paperwrk/safetycollabproject/accounts/SigninActivity.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/accounts/SigninActivity.java
@@ -1,7 +1,9 @@
 package in.paperwrk.safetycollabproject.accounts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import in.paperwrk.safetycollabproject.R;
 
@@ -11,5 +13,10 @@ public class SigninActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+    }
+    public void moveSignUpPage(View v){
+        Intent signin = new Intent(this,SignupActivity.class);
+        startActivity(signin);
     }
 }
