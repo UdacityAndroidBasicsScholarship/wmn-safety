1ab35ca179bf53e8e51ad86e915d952e1003e902
Akash Srivastava <akashsrivastava995@gmail.com>
2018-04-30 01:40:50 +0530

    Organized activity flow from one to another
    Arranged all the activity and inter connected them to each other.
    Added intents to  access other activities on clicks.

--- app/src/main/java/in/paperwrk/safetycollabproject/accounts/SignupActivity.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/accounts/SignupActivity.java
@@ -1,7 +1,10 @@
 package in.paperwrk.safetycollabproject.accounts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import in.paperwrk.safetycollabproject.R;
 
@@ -11,5 +14,13 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        TextView already=(TextView) findViewById(R.id.already_have_acc);
+        already.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(SignupActivity.this,SigninActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
