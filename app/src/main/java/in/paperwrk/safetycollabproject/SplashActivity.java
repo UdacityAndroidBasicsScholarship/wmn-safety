d19f9a0d54db690c5b2a3ff8b6fef9e995a88d9c
shiv-u <shivshankar.hc@gmail.com>
2018-04-29 18:30:02 +0530

    I have added login and sign up fragment . I have also made some minor changes in splash activity.

--- app/src/main/java/in/paperwrk/safetycollabproject/SplashActivity.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/SplashActivity.java
@@ -11,15 +11,11 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
         // go to MainActivity after a delay 2 seconds
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
         },2000);
 
 
