5b369977d4a2820f8783032828e3947d0dbd193f
la2798 <38189248+la2798@users.noreply.github.com>
2018-04-30 17:42:06 +0530

    Changed home,signin, signup and forgotPassword activity. Added a new logo

--- app/src/main/java/in/paperwrk/safetycollabproject/HomeActivity.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/HomeActivity.java
@@ -1,13 +1,48 @@
 package in.paperwrk.safetycollabproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity {
+import in.paperwrk.safetycollabproject.accounts.SigninActivity;
+import in.paperwrk.safetycollabproject.accounts.SignupActivity;
 
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button signin;
+    private Button signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        signin = findViewById(R.id.btsignin);
+        signup = findViewById(R.id.btsignup);
+
+        signin.setOnClickListener(this);
+        signup.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.btsignin:
+                Intent intent = new Intent(HomeActivity.this, SigninActivity.class);
+                startActivity(intent);
+
+                break;
+            case R.id.btsignup:
+                Intent intent1 = new Intent(HomeActivity.this, SignupActivity.class);
+                startActivity(intent1);
+
+                break;
+        }
+    }
+
+
 }
--- app/src/main/java/in/paperwrk/safetycollabproject/accounts/SigninActivity.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/accounts/SigninActivity.java
@@ -1,15 +1,68 @@
 package in.paperwrk.safetycollabproject.accounts;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import in.paperwrk.safetycollabproject.HomeActivity;
 import in.paperwrk.safetycollabproject.R;
 
 public class SigninActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+    private ImageButton signin;
+    private TextView forgotpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        signin = findViewById(R.id.btsignin);
+        email = (EditText)findViewById(R.id.etemail_id);
+        password = (EditText)findViewById(R.id.etpassword);
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(email.getText().toString(),password.getText().toString());
+            }
+        });
+        forgotpassword=(TextView)findViewById(R.id.forgotpassword);
+
+        String text = "Forgot Password?";
+        SpannableString ss = new SpannableString(text);
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                Intent intent = new Intent(SigninActivity.this, ForgotPasswordActivity.class);
+                startActivity(intent);
+            }
+        };
+        ss.setSpan(clickableSpan,0,16, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        forgotpassword.setText(ss);
+        forgotpassword.setMovementMethod(LinkMovementMethod.getInstance());
     }
+
+    private void validate (String userName , String userPassword) {
+        if ((userName.equals("admin@gmail.com")) && (userPassword.equals("1234"))) {
+            Intent intent = new Intent(SigninActivity.this, SigninActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "Invalid email-id/password", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
