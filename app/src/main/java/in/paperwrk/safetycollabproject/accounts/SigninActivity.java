422f7fcd3a920835a40532e5486626723dad6fa4
sabdar18 <developer.sabdar@gmail.com>
2018-04-30 20:27:52 +0530

    Add Login Logic(email and password firebase auth method) and Signin with google login with validation messages

--- app/src/main/java/in/paperwrk/safetycollabproject/accounts/SigninActivity.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/accounts/SigninActivity.java
@@ -1,29 +1,72 @@
 package in.paperwrk.safetycollabproject.accounts;
 
 import android.content.Intent;
+import android.print.PrinterId;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.signin.SignIn;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
+import in.paperwrk.safetycollabproject.HomeActivity;
 import in.paperwrk.safetycollabproject.R;
+import in.paperwrk.safetycollabproject.utilities.Helpers;
 
 public class SigninActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = SigninActivity.class.getSimpleName();
+    private static final int RC_SIGN_IN = 1011;
 
     private SignInButton mGoogleSignInButton;
     private Button mSignInButton;
     private TextView mSignUpTextView;
     private TextView mForgotPasswordTextView;
+    private FirebaseAuth mFirebaseAuth;
+    private GoogleSignInClient mGoogleSigninClient;
+    private TextInputEditText mEmailText;
+    private TextInputEditText mPasswordText;
+    private Helpers mHelpers;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSigninClient = GoogleSignIn.getClient(this, gso);
+
+        mHelpers = new Helpers(this);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mFirebaseAuth.getCurrentUser();
+        if (user != null) {
+            navigateToHome();
+        }
         mGoogleSignInButton = findViewById(R.id.sign_in_goggle_button);
         mGoogleSignInButton.setSize(SignInButton.SIZE_WIDE);
         mGoogleSignInButton.setColorScheme(SignInButton.COLOR_DARK);
@@ -31,6 +74,8 @@ public class SigninActivity extends AppCompatActivity implements View.OnClickLis
         mSignInButton = findViewById(R.id.sign_in_button);
         mSignUpTextView = findViewById(R.id.sign_in_register_tv);
         mForgotPasswordTextView = findViewById(R.id.sign_in_forgot_pass_tv);
+        mEmailText = findViewById(R.id.sign_in_email_tv);
+        mPasswordText = findViewById(R.id.sign_in_password_tv);
 
 
         mGoogleSignInButton.setOnClickListener(this);
@@ -38,6 +83,7 @@ public class SigninActivity extends AppCompatActivity implements View.OnClickLis
         mSignUpTextView.setOnClickListener(this);
         mForgotPasswordTextView.setOnClickListener(this);
     }
+
 
     @Override
     public void onClick(View v) {
@@ -72,10 +118,91 @@ public class SigninActivity extends AppCompatActivity implements View.OnClickLis
     }
 
     private void doGoogleSignIn() {
-        Toast.makeText(this, R.string.implementation_in_progress, Toast.LENGTH_SHORT).show();
+        Intent googleSignInIntent = mGoogleSigninClient.getSignInIntent();
+        startActivityForResult(googleSignInIntent, RC_SIGN_IN);
     }
 
     private void doSignIn() {
-        Toast.makeText(this, R.string.implementation_in_progress, Toast.LENGTH_SHORT).show();
+        String email = mEmailText.getText().toString();
+        String password = mPasswordText.getText().toString();
+
+        if(email.isEmpty()){
+            mHelpers.showAlertDialog(getString(R.string.email_required_msg),getString(R.string.enter_email_msg)).show();
+            return;
+        }
+
+        if(password.isEmpty()){
+            mHelpers.showAlertDialog(getString(R.string.password_required_msg),getString(R.string.enter_password_msg)).show();
+            return;
+        }
+        firebaseEmailPassword(email, password);
     }
+
+    private void navigateToHome() {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            firebaseAuthWithGoogle(account);
+            Log.i(TAG, account.getDisplayName());
+
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            String errorMessage = new Helpers(this).getErrorMessage(e.getStatusCode());
+            Log.w(TAG, "TAG:failed code=" + e.getStatusCode());
+            Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mFirebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            // Signed in successfully, show authenticated UI.
+                            navigateToHome();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.d(TAG, "signInWithCredential:failure"+ task.getException().getMessage());
+                        }
+                    }
+                });
+    }
+
+    private void firebaseEmailPassword(String email, String password){
+        mHelpers.showProgressDialog(getString(R.string.loading_msg));
+        mFirebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        mHelpers.hideProgressDialog();
+                        if(task.isSuccessful()){
+                            Log.d(TAG, task.getResult().getUser().getEmail());
+                        }else{
+                          mHelpers.showAlertDialog("Error Message", task.getException().getMessage()).show();
+                        }
+                    }
+                });
+    }
+
 }
