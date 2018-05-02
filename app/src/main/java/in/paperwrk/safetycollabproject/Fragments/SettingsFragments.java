e7e4eff372c253f749a62e89a623ed3e3baab21c
Ajay Mourya <ajaymourya8553@gmail.com>
2018-05-02 18:37:54 +0530

    Designed and implemented app navigation drawer menu and added the required sample fragments

---
+++ app/src/main/java/in/paperwrk/safetycollabproject/Fragments/SettingsFragments.java
@@ -0,0 +1,34 @@
+package in.paperwrk.safetycollabproject.Fragments;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import in.paperwrk.safetycollabproject.R;
+
+public class SettingsFragments extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_settings,null);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(),"you are in the settings fragment",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/in/paperwrk/safetycollabproject/Fragments/TrustedContactsFragment.java
@@ -0,0 +1,35 @@
+package in.paperwrk.safetycollabproject.Fragments;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import in.paperwrk.safetycollabproject.R;
+
+public class TrustedContactsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_trusted_contacts,null);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(),"you are in the Trusted contact fragment",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+}
