c2f90c556771a84d32dc16efb52605dd70055d75
Nitish Kumar Jha <njha6185@gmail.com>
2018-05-04 01:29:58 +0530

    Backend for SignIn SignUp added and connected with firebase auth

--- app/src/main/java/in/paperwrk/safetycollabproject/accounts/SigninActivity.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/accounts/SigninActivity.java
@@ -2,6 +2,7 @@ package in.paperwrk.safetycollabproject.accounts;
 
 import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Patterns;
@@ -11,8 +12,16 @@ import android.view.animation.AnticipateOvershootInterpolator;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
+import in.paperwrk.safetycollabproject.MainActivity;
 import in.paperwrk.safetycollabproject.R;
 
 public class SigninActivity extends AppCompatActivity {
@@ -22,6 +31,8 @@ public class SigninActivity extends AppCompatActivity {
     private Button loginButton, registerButton, forgotPasswordButton;
     private String valid_email, valid_password;
     private long duration = 5000;
+    private FirebaseAuth mFirebaseAuth;
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +46,8 @@ public class SigninActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (check_validity()) {
-                    Toast.makeText(SigninActivity.this, " Login Successfully !!! ", Toast.LENGTH_SHORT).show();
+                    user_login();
+//                    Toast.makeText(SigninActivity.this, " Login Successfully !!! ", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -59,12 +71,35 @@ public class SigninActivity extends AppCompatActivity {
         });
     }
 
+    public void user_login() {
+        mProgressBar.setVisibility(View.VISIBLE);
+        mFirebaseAuth.signInWithEmailAndPassword(valid_email, valid_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(SigninActivity.this, " Login Successful", Toast.LENGTH_SHORT).show();
+                    mProgressBar.setVisibility(View.GONE);
+                    Intent intent = new Intent(SigninActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    // Toast.makeText(SignupActivity.this, "Please Login!!!", Toast.LENGTH_LONG).show();
+                    finish();
+                } else {
+                    mProgressBar.setVisibility(View.GONE);
+                    Toast.makeText(SigninActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
     public void init() {
         emailEditext = (EditText) findViewById(R.id.email_edittext);
         passwordEditext = (EditText) findViewById(R.id.password_editetxt);
         loginButton = (Button) findViewById(R.id.login_button);
         registerButton = (Button) findViewById(R.id.register_button);
         forgotPasswordButton = (Button) findViewById(R.id.forgot_password_button);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
     }
 
     public void flip_logo() {
--- app/src/main/java/in/paperwrk/safetycollabproject/accounts/SignupActivity.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/accounts/SignupActivity.java
@@ -2,6 +2,7 @@ package in.paperwrk.safetycollabproject.accounts;
 
 import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Patterns;
@@ -11,7 +12,14 @@ import android.view.animation.AnticipateOvershootInterpolator;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 
 import in.paperwrk.safetycollabproject.R;
 
@@ -22,6 +30,8 @@ public class SignupActivity extends AppCompatActivity {
     private Button registerButton;
     private String valid_name, valid_email, valid_phone, valid_password;
     private long duration = 5000;
+    private FirebaseAuth mFirebaseAuth;
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,13 +45,36 @@ public class SignupActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (check_validity()) {
-                    Intent intent = new Intent(SignupActivity.this, SigninActivity.class);
-                    startActivity(intent);
-                    Toast.makeText(SignupActivity.this, "Please Login!!!", Toast.LENGTH_LONG).show();
-                    finish();
+                    registerUser();
                 }
             }
         });
+    }
+
+    public void registerUser() {
+        mProgressBar.setVisibility(View.VISIBLE);
+        mFirebaseAuth.createUserWithEmailAndPassword(valid_email, valid_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(SignupActivity.this, " Registration Successful", Toast.LENGTH_SHORT).show();
+                    mProgressBar.setVisibility(View.GONE);
+                    Intent intent = new Intent(SignupActivity.this, SigninActivity.class);
+                    startActivity(intent);
+                    // Toast.makeText(SignupActivity.this, "Please Login!!!", Toast.LENGTH_LONG).show();
+                    finish();
+                } else {
+                    if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                        Toast.makeText(SignupActivity.this, " User already Registered!!!", Toast.LENGTH_SHORT).show();
+                        mProgressBar.setVisibility(View.GONE);
+                    } else {
+                        Toast.makeText(SignupActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        mProgressBar.setVisibility(View.GONE);
+                    }
+                }
+            }
+        });
+
     }
 
     public void init() {
@@ -50,6 +83,8 @@ public class SignupActivity extends AppCompatActivity {
         mobileEditext = (EditText) findViewById(R.id.mobile_no_edittext);
         passwordEditext = (EditText) findViewById(R.id.password_editetxt);
         registerButton = (Button) findViewById(R.id.register_button);
+        mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        mFirebaseAuth = FirebaseAuth.getInstance();
     }
 
     public void flip_logo() {
