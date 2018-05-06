520630807e31717eae4a2a62684723d2e21ad844
Deepam Goel <deepamgoel4@gmail.com>
2018-05-06 23:10:41 +0530

    - Added BNV

--- app/src/main/java/in/paperwrk/safetycollabproject/HomeActivity.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/HomeActivity.java
@@ -2,6 +2,7 @@ package in.paperwrk.safetycollabproject;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -12,18 +13,25 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import java.util.Objects;
 
 import in.paperwrk.safetycollabproject.fragments.ExploreFragment;
+import in.paperwrk.safetycollabproject.fragments.FakeCallFragment;
 import in.paperwrk.safetycollabproject.fragments.FeedbackFragment;
 import in.paperwrk.safetycollabproject.fragments.HelpFragment;
 import in.paperwrk.safetycollabproject.fragments.SettingsFragment;
+import in.paperwrk.safetycollabproject.fragments.SosFragment;
+import in.paperwrk.safetycollabproject.fragments.TrackMeFragment;
 import in.paperwrk.safetycollabproject.fragments.TrustedContactFragment;
 
 public class HomeActivity extends AppCompatActivity {
 
     DrawerLayout mDrawerLayout;
+    FrameLayout mFrameLayout;
+    Fragment mFragment;
+    BottomNavigationView mBottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,42 +42,73 @@ public class HomeActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
 
         mDrawerLayout = findViewById(R.id.drawer_layout);
+        mBottomNavigationView = findViewById(R.id.navigation);
+        mFrameLayout = findViewById(R.id.bnv_content);
 
         ActionBar actionbar = getSupportActionBar();
         Objects.requireNonNull(actionbar).setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
 
-        final NavigationView navigationView = findViewById(R.id.nav_view);
+        mFragment = new SosFragment();
+        launchFragment(R.id.home_content, mFragment);
+
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.sos:
+                        mFragment = new SosFragment();
+                        launchFragment(R.id.home_content, mFragment);
+                        return true;
+                    case R.id.track_me:
+                        mFragment = new TrackMeFragment();
+                        launchFragment(R.id.home_content, mFragment);
+                        return true;
+                    case R.id.fake_call:
+                        mFragment = new FakeCallFragment();
+                        launchFragment(R.id.home_content, mFragment);
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                         menuItem.setChecked(true);
                         int id = menuItem.getItemId();
-                        Fragment fragment = null;
 
                         if (id == R.id.nav_trusted_contacts) {
-                            fragment = new TrustedContactFragment();
+                            mFragment = new TrustedContactFragment();
                         } else if (id == R.id.nav_explore) {
-                            fragment = new ExploreFragment();
+                            mFragment = new ExploreFragment();
                         } else if (id == R.id.nav_settings) {
-                            fragment = new SettingsFragment();
+                            mFragment = new SettingsFragment();
                         } else if (id == R.id.nav_help) {
-                            fragment = new HelpFragment();
+                            mFragment = new HelpFragment();
                         } else if (id == R.id.nav_feedback) {
-                            fragment = new FeedbackFragment();
+                            mFragment = new FeedbackFragment();
                         }
 
-                        if (fragment != null) {
-                            FragmentManager fragmentManager = getSupportFragmentManager();
-                            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                            fragmentTransaction.replace(R.id.home_content, fragment);
-                            fragmentTransaction.commit();
-                        }
+                        launchFragment(R.id.home_content, mFragment);
                         mDrawerLayout.closeDrawer(GravityCompat.START);
                         return true;
                     }
                 });
+    }
+
+    private void launchFragment(int id, Fragment fragment) {
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(id, fragment);
+            fragmentTransaction.commit();
+        }
     }
 
     @Override
