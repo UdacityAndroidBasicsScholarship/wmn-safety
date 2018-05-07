e6e2a789eb1e2b37b1b7c09553c1da88be42b994
OjasviBhargava <34980131+OjasviBhargava@users.noreply.github.com>
2018-05-07 16:30:31 +0530

    fixed bugs

--- app/src/main/java/in/paperwrk/safetycollabproject/utilities/PrefManager.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/utilities/PrefManager.java
@@ -8,25 +8,30 @@ import android.content.Context;
 import android.content.SharedPreferences;
 public class PrefManager {
 
-    SharedPreferences pref;
-    SharedPreferences.Editor editor;
-    Context context;
+    
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
 
-    public PrefManager(Context context){
-        this.context=context;
-        pref=context.getSharedPreferences("first",0);
-        editor=pref.edit();
+    // Shared preferences file name
+    private static final String PREF_NAME = "collab-project";
 
+    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+
+    public PrefManager(Context context) {
+        int PRIVATE_MODE = 0;
+        pref = context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
     }
 
-    public void setFirst(Boolean isFirst){
-        editor.putBoolean("check",isFirst);
+    public void setFirstLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
         editor.commit();
     }
 
-    public boolean isFirstLaunch(){
-        return pref.getBoolean("check",true);
+    public boolean isFirstLaunch() {
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
     }
+
 }
 
 
