b56448ad71e17949f4d2affb0af1de9d5bb86518
Kartik Shandilya <kartikdroid17@gmail.com>
2018-05-09 17:15:10 +0530

    Revert "Added onBoarding"

--- app/src/main/java/in/paperwrk/safetycollabproject/SplashActivity.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/SplashActivity.java
@@ -2,10 +2,8 @@ package in.paperwrk.safetycollabproject;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -19,24 +17,15 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        final boolean isFirstTime = preferences.getBoolean(getString(R.string.pref_user_first_time), true);
         // go to MainActivity after a delay 2 seconds
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                if (isFirstTime) {
-                    Intent intent = new Intent(SplashActivity.this, OnBoardingActivity.class);
-                    startActivity(intent);
-                    finish();
-                }
-                else {
-                    Intent intent = new Intent(SplashActivity.this, SigninActivity.class);
-                    startActivity(intent);
-                    finish();
-                }
+                Intent intent = new Intent(SplashActivity.this, SigninActivity.class);
+                startActivity(intent);
+                finish();
             }
-        }, 2000);
+        },2000);
 
 
     }
