1f9444cd03a9eea87907bdb3cb4ca8ed0fde84ff
mantyshah <mantyshah31@gmail.com>
2018-05-11 16:30:00 +0530

    Completed designing layout of fragment_fake_call, fragment_sos, fragment_track_user. Added colors and strings as per the fragments. Added code for TrackUserFragment.java to find the current location of user. Added google api.

--- app/src/main/java/in/paperwrk/safetycollabproject/TrackUserFragment.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/TrackUserFragment.java
@@ -1,17 +1,42 @@
 package in.paperwrk.safetycollabproject;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class TrackUserFragment extends Fragment {
+public class TrackUserFragment extends Fragment implements OnMapReadyCallback {
+
+
+    SupportMapFragment mMapFragment;
+    private GoogleMap mMap;
+    double latitude;
+    double longitude;
+    private FusedLocationProviderClient mFusedLocationClient;
 
 
     public TrackUserFragment() {
@@ -23,7 +48,89 @@ public class TrackUserFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_track_user, container, false);
+        View v = inflater.inflate(R.layout.fragment_track_user, container, false);
+        mMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapView);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        if (mMapFragment == null) {
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            mMapFragment = SupportMapFragment.newInstance();
+            fragmentTransaction.replace(R.id.mapView, mMapFragment).commit();
+        }
+
+        mMapFragment.getMapAsync(this);
+        return v;
     }
 
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+        onPermissionCheck();
+
+    }
+
+    private void onPermissionCheck() {
+
+        if (Build.VERSION.SDK_INT < 23) {
+            if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            }
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if (location != null) {
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+                                LatLng cureentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                                mMap.addMarker(new MarkerOptions().position(cureentLocation)
+                                        .title("Current Location")
+                                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cureentLocation, 15));
+                            }
+
+                        }
+                    });
+        } else {
+            if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                mFusedLocationClient.getLastLocation()
+                        .addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+                            @Override
+                            public void onSuccess(Location location) {
+
+                                if (location != null) {
+                                    latitude = location.getLatitude();
+                                    longitude = location.getLongitude();
+                                    LatLng cureentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                                    mMap.addMarker(new MarkerOptions().position(cureentLocation).title("Current Location"));
+                                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cureentLocation, 15));
+                                }
+
+                            }
+                        });
+
+            } else {
+                mFusedLocationClient.getLastLocation()
+                        .addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+                            @Override
+                            public void onSuccess(Location location) {
+
+                                if (location != null) {
+                                    latitude = location.getLatitude();
+                                    longitude = location.getLongitude();
+                                    LatLng cureentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                                    mMap.addMarker(new MarkerOptions().position(cureentLocation).title("Current Location"));
+                                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cureentLocation, 15));
+                                }
+
+                            }
+                        });
+            }
+        }
+    }
 }
