a7ec2b249591f9cd0e781562075c4d3e200caf0c
Kartik <kartikdroid17@gmail.com>
2018-05-13 18:15:36 +0530

    Added YouTube Jar Library. Created Explore Activity and layout for video card

---
+++ app/src/main/java/in/paperwrk/safetycollabproject/activities/ExploreActivity.java
@@ -0,0 +1,25 @@
+package in.paperwrk.safetycollabproject.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import in.paperwrk.safetycollabproject.R;
+
+public class ExploreActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explore);
+
+
+
+    }
+
+
+
+
+
+}
