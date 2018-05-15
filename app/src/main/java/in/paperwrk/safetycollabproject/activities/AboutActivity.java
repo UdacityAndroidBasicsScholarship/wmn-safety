21dc2a30451547e0747932af6e174bfe2aca68f2
Kartik <kartikdroid17@gmail.com>
2018-05-15 23:42:31 +0530

    Added `AboutActivity`

--- app/src/main/java/in/paperwrk/safetycollabproject/activities/AboutActivity.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/activities/AboutActivity.java
@@ -1,10 +1,13 @@
 package in.paperwrk.safetycollabproject.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import in.paperwrk.safetycollabproject.R;
+import mehdi.sakout.aboutpage.AboutPage;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -19,6 +22,24 @@ public class AboutActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         setTitle("About");
+
+        AppBarLayout appBarLayout;
+
+
+        appBarLayout = findViewById(R.id.appBarAnim);
+
+
+
+        View aboutPage = new AboutPage(this)
+                .isRTL(false)
+                .setImage(R.mipmap.ic_launcher)
+                .setDescription(getString(R.string.about_us_description))
+                .addGroup("Find out More")
+                .addEmail("pepperfriday1010@gmail.com")
+                .addGitHub("UdacityAndroidBasicsScholarship/wmn-safety")
+                .create();
+
+        appBarLayout.addView(aboutPage,-1);
 
 
     }
