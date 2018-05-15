4fc2e1043d87e86c622d167b849ec60659cf2e52
Kartik <kartikdroid17@gmail.com>
2018-05-15 10:54:40 +0530

    Removed `About` and `Help and FAQs` from Settings

--- app/src/main/java/in/paperwrk/safetycollabproject/SafetyApplication.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/SafetyApplication.java
@@ -1,30 +1,25 @@
 package in.paperwrk.safetycollabproject;
 
 import android.app.Application;
-import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 
 import com.squareup.leakcanary.LeakCanary;
-import com.squareup.leakcanary.RefWatcher;
 
 public class SafetyApplication extends Application{
 
-    public RefWatcher refWatcher;
+    //public RefWatcher refWatcher;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        initializeLeakCanary();
+        //initializeLeakCanary();
     }
 
     private void initializeLeakCanary() {
         if (LeakCanary.isInAnalyzerProcess(this)) {
             return;
         }
-        refWatcher = LeakCanary.install(this);
+        //refWatcher = LeakCanary.install(this);
     }
 
 }
