f7cc9e88171685b095d23cca5c462966f4aa44ee
Kartik <kartikdroid17@gmail.com>
2018-05-15 11:05:44 +0530

    Removed redundant FCM code

--- app/src/main/java/in/paperwrk/safetycollabproject/fragments/FakeCallFragment.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/fragments/FakeCallFragment.java
@@ -10,9 +10,6 @@ import android.view.ViewGroup;
 import in.paperwrk.safetycollabproject.R;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class FakeCallFragment extends Fragment {
 
 
