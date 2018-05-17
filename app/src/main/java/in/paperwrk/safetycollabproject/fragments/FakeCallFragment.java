d0ca18a7e8fb7d09140e59f4ee9e0d3360abe9fe
Kartik <kartikdroid17@gmail.com>
2018-05-17 22:30:54 +0530

    updated `About Us`

--- app/src/main/java/in/paperwrk/safetycollabproject/fragments/FakeCallFragment.java
+++ app/src/main/java/in/paperwrk/safetycollabproject/fragments/FakeCallFragment.java
@@ -1,16 +1,23 @@
 package in.paperwrk.safetycollabproject.fragments;
 
 
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import in.paperwrk.safetycollabproject.R;
 
 
-public class FakeCallFragment extends Fragment {
+public class FakeCallFragment extends Fragment implements View.OnClickListener{
+
+
+    ImageButton fakeCallButton;
 
 
     public FakeCallFragment() {
@@ -21,8 +28,21 @@ public class FakeCallFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fake_call, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_fake_call, container, false);
+
+        fakeCallButton = view.findViewById(R.id.fragment_fake_call_button);
+        fakeCallButton.setOnClickListener(this);
+
+        return view;
     }
 
+
+    @Override
+    public void onClick(View v) {
+
+        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+        Ringtone ringtone = RingtoneManager.getRingtone(getActivity(),notification);
+        ringtone.play();
+    }
 }
